9ef9b1b447929d38438f420dd56d2419febd6889
Kanersps <kanepetra@gmail.com>
2021-03-10 10:47:32 +0100

    fix: add migration files

---
+++ ringkey.Data/Migrations/20210305172636_InitialCreation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ringkey.Data.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Title = table.Column<string>(type: "longtext", nullable: true),
+                    Author = table.Column<string>(type: "longtext", nullable: true),
+                    Content = table.Column<string>(type: "longtext", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Parent = table.Column<string>(type: "longtext", nullable: true),
+                    Created = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
