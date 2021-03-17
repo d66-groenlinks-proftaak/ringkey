70b1cabfcefd9b4404f880638d4b6be90544b4b1
Kanersps <kanepetra@gmail.com>
2021-03-17 10:41:18 +0100

    fix: add migrations

---
+++ ringkey.Data/Migrations/20210316115223_AddMessageTags.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ringkey.Data.Migrations
+{
+    public partial class AddMessageTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Role",
+                type: "char(36)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Pinned",
+                table: "Message",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: true),
+                    MessageId = table.Column<Guid>(type: "char(36)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Icon = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tag_Message_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Message",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_MessageId",
+                table: "Tag",
+                column: "MessageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropColumn(
+                name: "Pinned",
+                table: "Message");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Role",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)");
+        }
+    }
+}
---
+++ ringkey.Data/Migrations/20210316140750_AddMessageViews.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ringkey.Data.Migrations
+{
+    public partial class AddMessageViews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Views",
+                table: "Message",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Views",
+                table: "Message");
+        }
+    }
+}
