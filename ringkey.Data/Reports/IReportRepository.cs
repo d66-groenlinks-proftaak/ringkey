4f6b7d98cfb67f44eb681ec81c72b2a9818cd7e4
Jasper Meiracker <362948@student.fontys.nl>
2021-03-18 14:18:59 +0100

    Reported dude

---
+++ ringkey.Data/Reports/IReportRepository.cs
@@ -0,0 +1,14 @@
+﻿using ringkey.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ringkey.Data.Reports
+{
+    public interface IReportRepository : IRepository<Report>
+    {
+
+    }
+}
