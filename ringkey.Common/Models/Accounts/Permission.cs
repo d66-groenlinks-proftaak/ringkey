f9d8849df888437d1fafdacfad5d3b160c2107a9
Jasper Meiracker <362948@student.fontys.nl>
2021-03-25 13:28:16 +0100

    feature: added permissions

---
+++ ringkey.Common/Models/Accounts/Permission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ringkey.Common.Models.Accounts
+{
+    public class Permission
+    {
+        public Permissions Perm {get;set;}
+    }
+
+}
---
+++ ringkey.Common/Models/Accounts/Permissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ringkey.Common.Models.Accounts
+{
+    public enum Permissions
+    {
+        Ban,
+        Categorize,
+        Quiz,
+        Webinar,
+        Announcement
+    }
+}
