96fbecc2c06bbaa287bc3839eab05b9f3fab9fd8
Kanersps <kanepetra@gmail.com>
2021-04-08 10:55:25 +0200

    tweak: add locked migration

---
+++ ringkey.Data/Migrations/20210408085407_AddLocked.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ringkey.Data.Migrations
+{
+    public partial class AddLocked : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "locked",
+                table: "Message",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "locked",
+                table: "Message");
+        }
+    }
+}
