fe9cb9dd37d4d14756529aaa48d702bff64a3ba0
Kanersps <kanepetra@gmail.com>
2021-05-20 10:24:04 +0200

    tweak: additional requirements

--- ringkey.Common/Models/Accounts/AccountError.cs
+++ ringkey.Common/Models/Accounts/AccountError.cs
@@ -2,16 +2,17 @@
 {
     public enum AccountError
     {
-        NoError,
-        InvalidEmail,
-        FirstNameTooShort,
-        FirstNameTooLong,
-        LastNameTooShort,
-        LastNameTooLong,
-        PasswordTooShort,
-        PasswordTooLong,
-        EmailInUse,
-        InvalidCaptcha,
-        InvalidLogin
+        NoError = 0,
+        InvalidEmail = 1,
+        FirstNameTooShort = 2,
+        FirstNameTooLong = 3,
+        LastNameTooShort = 4,
+        LastNameTooLong = 5,
+        PasswordTooShort = 6,
+        PasswordTooLong = 7,
+        EmailInUse = 8,
+        InvalidCaptcha = 9,
+        InvalidLogin = 10,
+        InvalidPasswordCharacters = 11
     }
 }
